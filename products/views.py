2a2ff6274a65cc003ba21c897c228eb18571028c
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-09-27 17:42:29 -0600

    saving informations

--- products/views.py
+++ products/views.py
@@ -6,10 +6,6 @@ class ListProductsView(TemplateView):
     template_name = "products/list_products.html"
     
     def get_context_data(self, **kwargs):
-        """"
-        context = super().get_context_data(**kwargs)
-        author_id = self.kwargs.get('id')
-        """
         context = super().get_context_data(**kwargs)
         products = Product.objects.all()
         context['list_products'] = products
