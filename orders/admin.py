5d368cf8073df426964a49e22d03bade5a71c007
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-09-28 11:01:06 -0600

    new application orders

---
+++ orders/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Order, OrderProduct
+
+
+class OrderProductInlineAdmin(admin.TabularInline):
+    model = OrderProduct
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [OrderProductInlineAdmin]
+    list_display = ('user', 'is_active', 'created_at', 'updated_at')
+    search_fields = ('user',)  
+
+admin.site.register(Order, OrderAdmin)
---
+++ orders/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"order {self.id} by {self.user}"
+    
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"order product {self.id} for {self.order} with {self.product} and {self.quantity} quantity"
+
