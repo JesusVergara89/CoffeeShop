7530aaec6d6b3995d0cbbf1b52c5df740574673c
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-09-27 18:17:29 -0600

    adding a form to create products form view

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListProductsView
+from .views import ListProductsView, ProductFormView
 
 urlpatterns = [
     path('', ListProductsView.as_view(), name='list'),
+    path('add/', ProductFormView.as_view(), name='add'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from .models import Product
+from django.views import generic
+from .forms import ProductForm
 
 class ListProductsView(TemplateView):
     template_name = "products/list_products.html"
@@ -10,3 +13,15 @@ class ListProductsView(TemplateView):
         products = Product.objects.all()
         context['list_products'] = products
         return context
+    
+class ProductFormView(generic.FormView):
+    template_name = "products/add_products.html"
+
+    form_class = ProductForm
+    success_url = reverse_lazy('add')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+
