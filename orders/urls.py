1d007aa2d2ab47d8397ef6506ed4081e3a5ff7db
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-09-28 15:21:12 -0600

    adding products in prders and install django rest fram work

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OrderDetailView
+from .views import OrderDetailView, AddProductView
 
 urlpatterns = [
    path('myorder/', OrderDetailView.as_view(), name='order_detail'),
+   path('addproduct/', AddProductView.as_view(), name='add_product'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView
 from .models import Order
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import OrderForm
 
 class OrderDetailView(LoginRequiredMixin, DetailView):
     model = Order
@@ -8,5 +10,20 @@ class OrderDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'order'
     def get_object(self, queryset=None):
         return Order.objects.filter(user=self.request.user, is_active=True).first()
+    
+class AddProductView(LoginRequiredMixin, CreateView):
+    template_name = "orders/add_product.html"
+    form_class = OrderForm
+    success_url = reverse_lazy('order_detail')
+
+    def form_valid(self, form):
+        order, _ = Order.objects.get_or_create(
+            is_active=True,
+            user=self.request.user,
+        )
+        form.instance.order = order
+        form.instance.quantity = 1
+        form.save()
+        return super().form_valid(form)
 
 
