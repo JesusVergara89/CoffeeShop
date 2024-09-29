84f29a0301a57a7b3d906a8ffb734b7084147cdb
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-09-28 19:02:28 -0600

    adding test to products

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Product
 
-# Create your tests here.
+class ListProductsViewTests(TestCase):
+
+    def test_should_return_200(self):
+        url = reverse('list')
+        response = self.client.get(url)
+        #breakpoint()
+        self.assertEqual(response.status_code, 200)
+        
+    def test_should_return_200_with_products(self):
+        url = reverse('list')
+        Product.objects.create(
+            name="test",
+            description="test description",
+            price=10,       
+            available=True,
+            image="test.png"
+        )
+        response = self.client.get(url)
+        #breakpoint()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['list_products'].count(), 1)
